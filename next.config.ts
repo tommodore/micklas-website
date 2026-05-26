7116d2b346608c9fb39270d4c0b13b71feb5bf1c
Thomas Kräuter <tommodore@users.noreply.github.com>
2026-05-26 12:26:45 +0200

    Fix Payload bundling for Next.js 15

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['payload'],
-  },
+  serverExternalPackages: ['payload'],
   images: {
     remotePatterns: [
       {
